a700ae086249e5304d1b45924339153c603addf6
bikehubtech <johnatcivil@gmail.com>
2026-09-02 20:22:29 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { applyGnexisTheme } from "./config/theme";
+import { ToastProvider } from "./contexts";
 import "./styles/global.css";
 
 applyGnexisTheme();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </React.StrictMode>
 );
