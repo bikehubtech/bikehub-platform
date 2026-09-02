b1d1af42841e2239993d735f6d20b7931c7f9d0f
bikehubtech <johnatcivil@gmail.com>
2026-09-02 19:44:26 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { applyBikeHubTheme } from "./config/theme";
+import { applyGnexisTheme } from "./config/theme";
 import "./styles/global.css";
 
-applyBikeHubTheme();
+applyGnexisTheme();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
