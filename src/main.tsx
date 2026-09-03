9b7ce4c4acfbe971b91944fdf19eb9297463b594
bikehubtech <johnatcivil@gmail.com>
2026-09-02 21:00:45 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { applyGnexisTheme } from "./config/theme";
-import { ToastProvider } from "./contexts";
+import { AuthProvider, ToastProvider } from "./contexts";
 import "./styles/global.css";
 
 applyGnexisTheme();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ToastProvider>
-      <App />
-    </ToastProvider>
+    <AuthProvider>
+      <ToastProvider>
+        <App />
+      </ToastProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
